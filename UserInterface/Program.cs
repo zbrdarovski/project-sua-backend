d64e04d1181cfd72bb0ad5346f62ca253dbc652e
Zdravko Brdarovski <zbrdarovski@gmail.com>
2024-06-15 15:56:53 +0200

    Remove proxy server from frontend

--- UserInterface/Program.cs
+++ UserInterface/Program.cs
@@ -1,26 +1,64 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+Process npmProcess = null;
+
+// Run npm start in the ClientApp folder if in Development environment and not in debug mode
+if (app.Environment.IsDevelopment() && !Debugger.IsAttached)
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    var clientAppPath = Path.Combine(Directory.GetCurrentDirectory(), "ClientApp");
+    if (Directory.Exists(clientAppPath))
+    {
+        var npmStart = new ProcessStartInfo
+        {
+            FileName = "cmd",
+            Arguments = "/c npm start",
+            WorkingDirectory = clientAppPath,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        npmProcess = Process.Start(npmStart);
+        npmProcess.OutputDataReceived += (sender, args) => Console.WriteLine(args.Data);
+        npmProcess.BeginOutputReadLine();
+        npmProcess.ErrorDataReceived += (sender, args) => Console.WriteLine(args.Data);
+        npmProcess.BeginErrorReadLine();
+
+        app.Lifetime.ApplicationStopping.Register(() =>
+        {
+            if (npmProcess != null && !npmProcess.HasExited)
+            {
+                npmProcess.Kill();
+                npmProcess.Dispose();
+            }
+        });
+    }
+    else
+    {
+        Console.WriteLine($"ClientApp directory not found: {clientAppPath}");
+    }
 }
 
+// Configure the HTTP request pipeline.
 app.UseStaticFiles();
 app.UseRouting();
-
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller}/{action=Index}/{id?}");
 
-app.MapFallbackToFile("index.html"); ;
+app.MapFallbackToFile("index.html");
 
 app.Run();
