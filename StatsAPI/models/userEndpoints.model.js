74d7a9f9cfbf795808385b61c7ea4f4d4d622422
Zdravko Brdarovski <zbrdarovski@gmail.com>
2024-07-17 15:19:19 +0200

    Delete unnecessary project folders

--- StatsAPI/models/userEndpoints.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const statsEndpointsSchema = new mongoose.Schema({
-    timestamp: Date,
-    endpoint: String,
-});
-
-const StatsEndpoints = mongoose.model('UserEndpoints', statsEndpointsSchema);
-module.exports = StatsEndpoints;
--- StatzAPI/StatsData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StatzAPI.Models
-{
-    public class StatsData
-    {
-        public string Endpoint { get; set; }
-        public int CallCount { get; set; }
-        public DateTime LastAccessed { get; internal set; }
-    }
-}
