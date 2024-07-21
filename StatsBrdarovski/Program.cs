ca807d7cf45775856871f8e53bef51611b564b3b
Zdravko Brdarovski <zbrdarovski@gmail.com>
2024-07-21 15:54:05 +0200

    Remove unnecessary project files and update NuGet packages

--- StatsBrdarovski/Program.cs
+++ StatsBrdarovski/Program.cs
@@ -1,4 +1,5 @@
 using StatsBrdarovski;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,13 +10,40 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+string? environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
 builder.Services.AddSingleton<StatsRepository>(serviceProvider =>
 {
-    var mongoDbConnectionString = builder.Configuration.GetConnectionString("MongoDBConnection");
+    string mongoDbConnectionString;
+
+    if (environment == "Development")
+    {
+        // In Development, use the connection string from appsettings.json
+        mongoDbConnectionString = builder.Configuration.GetConnectionString("MongoDBConnection");
+    }
+    else
+    {
+        // In non-Development, use the environment variable
+        mongoDbConnectionString = Environment.GetEnvironmentVariable("MONGODB_CONNECTION_STRING") ?? "mongodb+srv://sua-user:30SD8YKo4tg7R7v5@cluster0.550s6o6.mongodb.net/?retryWrites=true&w=majority";
+    }
+
     return new StatsRepository(mongoDbConnectionString);
 });
 
+// Add CORS services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin() // Allow all origins
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 
@@ -26,5 +54,7 @@ app.UseSwaggerUI(c =>
 });
 
 app.MapControllers();
+
+app.UseCors("AllowAllOrigins");
 
 app.Run();
